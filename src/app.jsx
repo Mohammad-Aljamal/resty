e87f8c0cd7b1696bf49e3dc19388d70334233cde
Mohammad-Aljamal <mohammad.o.aljamal@gmail.com>
2023-08-14 15:04:31 +0000

    t

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+
+import { useState } from 'react';
+
+
 import './app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
@@ -9,15 +13,55 @@ import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
 
-class App extends React.Component {
+// class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+
+//   callApi = (requestParams) => {
+//     // mock output
+//     const data = {
+//       count: 2,
+//       results: [
+//         {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+//         {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+//       ],
+//     };
+//     this.setState({data, requestParams});
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <Header />
+//         <div>Request Method: {this.state.requestParams.method}</div>
+//         <div>URL: {this.state.requestParams.url}</div>
+//         <Form handleApiCall={this.callApi} />
+//         <Results data={this.state.data} />
+//         <Footer />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+
+function App (props) {
+
+//  constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: null,
+//       requestParams: {},
+//     };
+//   }
+
+    const [data, setData] = useState(null);
+    const [requestParams, setRequestParams] = useState({});
 
   callApi = (requestParams) => {
     // mock output
@@ -31,7 +75,6 @@ class App extends React.Component {
     this.setState({data, requestParams});
   }
 
-  render() {
     return (
       <React.Fragment>
         <Header />
@@ -42,7 +85,6 @@ class App extends React.Component {
         <Footer />
       </React.Fragment>
     );
-  }
 }
 
 export default App;
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 import './form.scss';
 
-class Form extends React.Component {
+// class Form extends React.Component {
 
-  handleSubmit = e => {
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const formData = {
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
+//     };
+//     this.props.handleApiCall(formData);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label >
+//             <span>URL: </span>
+//             <input name='url' type='text' />
+//             <button type="submit">GO!</button>
+//           </label>
+//           <label className="methods">
+//             <span id="get">GET</span>
+//             <span id="post">POST</span>
+//             <span id="put">PUT</span>
+//             <span id="delete">DELETE</span>
+//           </label>
+//         </form>
+//       </>
+//     );
+//   }
+// }
+
+function Form () {
+
+    handleSubmit = e => {
     e.preventDefault();
     const formData = {
       method:'GET',
@@ -13,25 +45,23 @@ class Form extends React.Component {
     this.props.handleApiCall(formData);
   }
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label >
-            <span>URL: </span>
-            <input name='url' type='text' />
-            <button type="submit">GO!</button>
-          </label>
-          <label className="methods">
-            <span id="get">GET</span>
-            <span id="post">POST</span>
-            <span id="put">PUT</span>
-            <span id="delete">DELETE</span>
-          </label>
-        </form>
-      </>
-    );
-  }
+return (
+        <>
+          <form onSubmit={this.handleSubmit}>
+            <label >
+              <span>URL: </span>
+              <input name='url' type='text' />
+              <button type="submit">GO!</button>
+            </label>
+            <label className="methods">
+              <span id="get">GET</span>
+              <span id="post">POST</span>
+              <span id="put">PUT</span>
+              <span id="delete">DELETE</span>
+            </label>
+          </form>
+        </>
+      );
 }
 
 export default Form;
